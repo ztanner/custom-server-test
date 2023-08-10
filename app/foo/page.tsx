3024db167ff99efd018b6cabd39354731beb06bf
Zack Tanner <zacktanner@gmail.com>
2023-08-09 17:02:24 -0700

    initial commit

---
+++ app/foo/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      Hello I am foo <Link href="/">To Index</Link>
+    </div>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default async function Page() {
+  console.log('testing a log')
+  return <div>Hello World</div>
+}
